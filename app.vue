eee0d19f3d6528be8842b8af5e57ebcab0aa51fb
Gautier <gautier.deleglise@gmail.com>
2023-08-30 22:43:15 +0200

    :bug: trailing slash flickering

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { withoutTrailingSlash } from 'ufo'
+
+const route = useRoute()
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  link: () => [
+    { rel: 'canonical', href: `https://laravel-rest-api.lomkit.com${withoutTrailingSlash(route.path)}` }
+  ]
+})
+</script>
+<template>
+  <AppLayout>
+    <NuxtPage />
+  </AppLayout>
+</template>
